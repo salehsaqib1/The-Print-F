1103092f4ddb800a2ccfb7bf36eff70768ee90e4
salehsaqib1 <saleh.fazal@mail.mcgill.ca>
2016-07-17 01:39:42 +0500

    Add files via upload

---
+++ A1_260674863.c
@@ -0,0 +1,24 @@
+/*
+ * A1_260674863.c
+ *
+ *  Created on: Jan 15, 2016
+ *      Author: Saleh Saqib Fazal
+ */
+
+//The text editor is Eclipse and the compiler is MinGW GCC
+
+#include <stdio.h>   //Include standard input output header
+int main (void)      //Main function to start the program. Void because no argument.
+{
+	printf("My name is Saleh Saqib Fazal and my mcgill ID is:\n\n");
+	printf("2\n");
+	printf(" 6\n");
+	printf("  0\n");
+	printf("   6\n");
+	printf("    7\n");
+	printf("     4\n");
+	printf("      8\n");
+	printf("       6\n");
+	printf("        3\n");
+	return 0;
+}
